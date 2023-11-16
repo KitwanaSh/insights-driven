9e647e509bb5cedd31114e026af98b30affcb150
KitwanaSh <rodriguekitweze@gmail.com>
2023-11-16 13:05:15 +0000

    Fix: Update the config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,6 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: [],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
   theme: {
     extend: {},
   },
